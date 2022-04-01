e98be1619e67848d65aa3d8bfdd425efaddc1e7c
MarcusStromz <marcusviniciusn2002@gmail.com>
2022-04-01 17:47:25 -0300

    1 file modified

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
-const  taxa  =  documento . getElementById ( 'inputTX' ) ; 
-const  real  =  documento . getElementById ( 'inputRD' ) ; 
-const  dolar  =  documento . getElementById ( 'inputDR' ) ; 
+const  taxa = documento.getElementById( 'inputTX' ) ; 
+const  real = documento.getElementById( 'inputRD' ) ; 
+const  dolar = documento.getElementById( 'inputDR' ) ; 
 
-const  bconfirmar  =  document . getElementById ( 'bconfirmar' ) ; 
-const  blimpar  =  documento . getElementById ( 'blimpar' ) ; 
-const  botaoRD  =  document . getElementById ( 'botaoRD' ) ; 
-const  botaoDR  =  document . getElementById ( 'botaoDR' ) ;
+const  bconfirmar = document.getElementById( 'bconfirmar' ) ; 
+const  blimpar = documento.getElementById( 'blimpar' ) ; 
+const  botaoRD = document.getElementById( 'botaoRD' ) ; 
+const  botaoDR = document.getElementById( 'botaoDR' ) ;
 
-botaoRD . desabilitado  =  verdadeiro ;
-botaoDR . desabilitado  =  verdadeiro ;
-blimpar . desabilitado  =  verdadeiro ;
-real . desabilitado  =  verdadeiro ;
-dólar . desabilitado  =  verdadeiro ;
-
-função;  confirmarTaxa ( )  ;{
+botaoRD.desabilitado = verdadeiro ;
+botaoDR.desabilitado = verdadeiro ;
+blimpar.desabilitado = verdadeiro ;
+real.desabilitado = verdadeiro ;
+dólar.desabilitado = verdadeiro ;
+função; confirmarTaxa ( )  ;{
   if  ( taxa . value  ===  '' )  {
-    return  alert ( 'Informe a taxax de conversão' ) ;
+    return alert ( 'Informe a taxa de conversão' ) ;
   }
-  taxa . desabilitado  =  verdadeiro ;
-  bconfirmar . desabilitado  =  verdadeiro ;
-  blimpar . desabilitado  =  falso ;
-  real . desabilitado  =  falso ;
-  dólar . desabilitado  =  falso ;
-  botaoRD . desabilitado  =  falso ;
-  botaoDR . desabilitado  =  falso ;
+  taxa.desabilitado = verdadeiro ;
+  bconfirmar.desabilitado = verdadeiro ;
+  blimpar.desabilitado = falso ;
+  real.desabilitado = falso ;
+  dólar.desabilitado = falso ;
+  botaoRD.desabilitado = falso ;
+  botaoDR.desabilitado = falso ;
 }
-função;  limpar ( )  ;{
-  taxa . valor  =  '' ;
-  bconfirmar . desabilitado  =  falso ;
-  botaoRD . desabilitado  =  verdadeiro ;
-  botaoDR . desabilitado  =  verdadeiro ;
-  taxa . desabilitado  =  falso ;
-  real . valor  =  '' ;
-  dólar . valor  =  '' ;
+função; limpar ( )  ;{
+  taxa.valor  =  '' ;
+  bconfirmar.desabilitado = falso ;
+  botaoRD.desabilitado =  verdadeiro ;
+  botaoDR.desabilitado = verdadeiro ;
+  taxa.desabilitado = falso ;
+  real.valor =  '' ;
+  dólar.valor =  '' ;
 }
-função;  conversorRD ( )  ;{
+função; conversorRD ( )  ;{
   deixe  ;dolares  =  0 ;
-  dólares  =  reais . valor  /  taxa . valor ;
-  consolo . log ( dolares ) ;
-  alerta  ( dolares  +  'dolares' ) ;
+  dólares = reais.valor  /  taxa.valor ;
+  consolo.log ( dolares ) ;
+  alerta ( dolares  +  'dolares' ) ;
 }
-função;  conversorDR ( )  ;{
-  deix  ;real  =  0 ;
-  real  =  dólar . valor  *  taxa . valor ;
-  consolo . log ( real ) ; 
+função; conversorDR ( )  ;{
+  deixe  ;real  =  0 ;
+  real  =  dólar . valor  *  taxa.valor ;
+  consolo.log ( real ) ; 
   alerta ( real  +  'reais' ) ;
 }
