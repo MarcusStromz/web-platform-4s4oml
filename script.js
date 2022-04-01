68bfeeac8703a7fea53df8bf2b54a73494a9d2c5
MarcusStromz <marcusviniciusn2002@gmail.com>
2022-04-01 12:42:21 -0300

    2 files modified

--- script.js
+++ script.js
@@ -1,51 +1,48 @@
-const  taxa  =  documento . getElementById ( 'inputTX' ) ;
-const  real  =  documento . getElementById ( 'inputRD' ) ;
-const  dolar  =  documento . getElementById ( 'inputDR' ) ;
+const  taxa  =  documento . getElementById ( 'inputTX' ) ; 
+const  real  =  documento . getElementById ( 'inputRD' ) ; 
+const  dolar  =  documento . getElementById ( 'inputDR' ) ; 
 
-const  botaoconfirmar  =  document . getElementById ( 'botaoconfirmar' ) ;
-const  botaolimpar  =  documento . getElementById ( 'botaolimpar' ) ;
-const  botaoRD  =  document . getElementById ( 'botaoRD' ) ;
+const  bconfirmar  =  document . getElementById ( 'bconfirmar' ) ; 
+const  blimpar  =  documento . getElementById ( 'blimpar' ) ; 
+const  botaoRD  =  document . getElementById ( 'botaoRD' ) ; 
 const  botaoDR  =  document . getElementById ( 'botaoDR' ) ;
 
 botaoRD . desabilitado  =  verdadeiro ;
 botaoDR . desabilitado  =  verdadeiro ;
-botaolimpar . desabilitado  =  verdadeiro ;
+blimpar . desabilitado  =  verdadeiro ;
 real . desabilitado  =  verdadeiro ;
 dólar . desabilitado  =  verdadeiro ;
 
-função  confirmaTaxa ( )  {
+função  ;confirmaTaxa ( )  ;{
   if  ( taxa . value  ===  '' )  {
     return  alert ( 'Digite a taxa de conversão para continuar' ) ;
   }
   taxa . desabilitado  =  verdadeiro ;
-  botaoconfirmar . desabilitado  =  verdadeiro ;
-  botaolimpar . desabilitado  =  falso ;
+  bconfirmar . desabilitado  =  verdadeiro ;
+  blimpar . desabilitado  =  falso ;
   real . desabilitado  =  falso ;
   dólar . desabilitado  =  falso ;
   botaoRD . desabilitado  =  falso ;
   botaoDR . desabilitado  =  falso ;
 }
-
-função  limpar ( )  {
+função  ;limpar ( )  ;{
   taxa . valor  =  '' ;
-  botaoconfirmar . desabilitado  =  falso ;
+  bconfirmar . desabilitado  =  falso ;
   botaoRD . desabilitado  =  verdadeiro ;
   botaoDR . desabilitado  =  verdadeiro ;
   taxa . desabilitado  =  falso ;
   real . valor  =  '' ;
   dólar . valor  =  '' ;
 }
-
-função  conversorRD ( )  {
-  deixe  dolares  =  0 ;
-  dolares  =  reais . valor  /  taxa . valor ;
-  consola . log ( dolares ) ;
-  alerta ( dolares  +  'dolares' ) ;
+função  ;conversorRD ( )  ;{
+  deixe  ;dolares  =  0 ;
+  dólares  =  reais . valor  /  taxa . valor ;
+  consolo . log ( dolares ) ;
+  alerta  ( dolares  +  'dolares' ) ;
 }
-
-função  conversorDR ( )  {
-  deixe  real  =  0 ;
+função  ;conversorDR ( )  ;{
+  deix  ;real  =  0 ;
   real  =  dólar . valor  *  taxa . valor ;
-  consola . log ( real ) ;
+  consolo . log ( real ) ; 
   alerta ( real  +  'reais' ) ;
 }
